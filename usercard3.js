95f5485faded374d5ea857ac77a90f80a168f8de
Gondela-Indhu <92864489+Gondela-Indhu@users.noreply.github.com>
2023-06-13 21:25:25 +0530

    typo

---
+++ usercard3.js
@@ -0,0 +1,27 @@
+let gandhioldinfo = {
+    imgurl: "https://tse2.mm.bing.net/th?id=OIP.h1u0WdtyHk1mITELUMvfPwHaEo&pid=Api&P=0&h=180",
+    name: "Mahatma Gandhi",
+    Description: "Freedom Fighter"
+}
+
+let gandhiyounginfo = {
+    imgurl: "https://tse3.mm.bing.net/th?id=OIP.NxIhSDYNWuiI7EHC7yu9QgHaHA&pid=Api&P=0&h=180",
+    name: "Mohandas KaramChandh Gandhi",
+    Description: "Father of the Nation"
+}
+
+let isgandhijiold = true;
+let displayobject;
+let flipdata = function(){
+    if(isgandhijiold == true){
+        displayobject = gandhiyounginfo;
+        isgandhijiold = false;
+    }
+    else{
+        displayobject = gandhioldinfo;
+        isgandhijiold = true;
+    }
+    document.getElementById("gandhi-img").src = displayobject.imgurl;
+    document.getElementById("mahatma-name").innerHTML = displayobject.name;
+    document.getElementById("mahatma-description").innerHTML = displayobject.Description;
+}
